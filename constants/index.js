d4ddd0346ae0bfe989b57234fcaa61f6f8ceee82
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2022-02-23 22:27:18 -0600

    smart contract ready

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xe39A622B08F279b71789BDE16EB70AC665Cd8fB5";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-pearl.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
